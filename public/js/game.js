657f7c8d9c8e1f1c1743e34aab2761226bfc105c
mMeneske <maksym0265@gmail.com>
2026-08-05 10:47:45 +0200

    moved all js code to the js folder

--- public/js/game.js
+++ public/js/game.js
@@ -144,39 +144,6 @@ function positionboxes(planeX, planeY) {
     lab_boxes.plane[planeX][planeY].size = gameScale / 20;
 }
 
-
-const registration = document.getElementById('registration');
-const login = document.getElementById('login');
-const playScreen = document.getElementById('start-screen');
-login.classList.remove('hidden');
-registration.classList.add('hidden');
-playScreen.classList.add('hidden');
-
-
-
-function register() {
-    login.classList.add('hidden');
-    registration.classList.remove('hidden');
-
-}
-
-function startScreen() {
-    login.classList.add('hidden');
-    playScreen.classList.remove('hidden');
-}
-
-
-
-function startGame() {
-
-    const startScreen = document.getElementById('start-screen');
-    login.classList.add('hidden');
-    registration.classList.add('hidden');
-    startScreen.classList.add('hidden');
-    isPlaying = true;
-
-}
-
 let last_time = 0;
 let tick_time = [];
 let tick_avg = 0;
