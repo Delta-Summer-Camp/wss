923b0fe9f14dd983d5eb55649442b7126f1a5a85
mMeneske <maksym0265@gmail.com>
2026-08-11 14:44:17 +0200

    Fixed crashing on the edge of world

--- public/js/game.js
+++ public/js/game.js
@@ -186,9 +186,11 @@ function positionboxes(currentX, currentY) {
     let planeY = currentY + currentScreenStartPos.y;
     lab_boxes.plane[currentX][currentY].x = player.x - pos.x * gameScale + planeX * gameScale * 2 + renderDesync.x * gameScale;
     lab_boxes.plane[currentX][currentY].y = player.y - pos.y * gameScale + planeY * gameScale * 2 + renderDesync.y * gameScale;
-    if (lab[planeY][planeX] == 0) {
+    if (planeY < 0 || planeY > 50 || planeX < 0 || planeX > 50) {
+        lab_boxes.plane[currentX][currentY].visible = true;
+    } else if (lab[planeY][planeX] == 0) {
         lab_boxes.plane[currentX][currentY].visible = false;
-    } else if (lab[planeY][planeX] == 1) {
+    } else {
         lab_boxes.plane[currentX][currentY].visible = true;
     }
     lab_boxes.plane[currentX][currentY].size = gameScale / 10;
